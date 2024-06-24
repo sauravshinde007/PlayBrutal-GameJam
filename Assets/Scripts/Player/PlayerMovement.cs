5c4f9cb25cdc869e82c1b5358936db8f534943a5
Saurav <sauravshinde.000@gmail.com>
2024-06-24 20:32:49 +0530

    added hover

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     [Header("Multiplayer")]
     [SerializeField] private PhotonView view;
@@ -9,7 +10,7 @@ public class PlayerMovement : MonoBehaviour {
     [Header("Movement Controls")]
     [SerializeField] float speed = 450f;
     [SerializeField] float jump_force = 10f;
-    [Range(0f, 1f)] [SerializeField] float stop_factor = 0.5f; // Controlled jump stopping factor
+    [Range(0f, 1f)][SerializeField] float stop_factor = 0.5f; // Controlled jump stopping factor
     [SerializeField] Transform feetPos; // feet position to check if grounded
     [SerializeField] Vector3 groundCheckSize = new Vector3(1, 0.05f, 1);
     [SerializeField] float gravity = 10; // gravity to apply when in air
@@ -20,7 +21,15 @@ public class PlayerMovement : MonoBehaviour {
     private float hangTimeCtr = 0;
     [SerializeField] private float jumpBufferLength = 0.1f;
     private float jumpBufferCtr = 0;
-    
+
+    [Header("Hover Controls")]
+    [SerializeField] float hoverForce = 5f;
+    [SerializeField] float maxFuel = 5f;
+    [SerializeField] float fuelConsumptionRate = 1f;
+    [SerializeField] float fuelRestoreRate = 0.5f;
+    private float currentFuel;
+    private bool isHovering = false;
+
     // Private variables
     private Rigidbody2D rb;
     private float movement; // movement input
@@ -28,46 +37,63 @@ public class PlayerMovement : MonoBehaviour {
     private bool can_jump = false;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        currentFuel = maxFuel;
     }
 
-    void GetInput(){
+    void GetInput()
+    {
         movement = Input.GetAxisRaw("Horizontal");
-        
+
         // Check if grounded by a physics check
         isGrounded = Physics2D.OverlapBoxAll(feetPos.position, groundCheckSize, 0f, whatIsGround).Length > 0;
         // Set gravity to 0 if on ground
         rb.gravityScale = isGrounded ? 0 : gravity;
 
         // Hang/Coyote Time
-        if(isGrounded){ hangTimeCtr = hangTime; }
+        if (isGrounded) { hangTimeCtr = hangTime; }
         else hangTimeCtr -= Time.deltaTime;
 
         // Jump Buffer
-        if(Input.GetButtonDown("Jump")) { jumpBufferCtr = jumpBufferLength; }
+        if (Input.GetButtonDown("Jump")) { jumpBufferCtr = jumpBufferLength; }
         else jumpBufferCtr -= Time.deltaTime;
 
         // Jump Detection
-        if(hangTimeCtr > 0f && jumpBufferCtr > 0){
+        if (hangTimeCtr > 0f && jumpBufferCtr > 0)
+        {
             can_jump = true;
             jumpBufferCtr = 0;
         }
-        
+
         // Controlled Jump
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0){
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * stop_factor);
+        }
+
+        // Hover Input
+        if (Input.GetKey(KeyCode.E) && currentFuel > 0)
+        {
+            isHovering = true;
+        }
+        else
+        {
+            isHovering = false;
         }
     }
 
     // Update is called once per frame
-    void Update() {
-        if(!view.IsMine) return;
+    void Update()
+    {
+        if (!view.IsMine) return;
         GetInput();
     }
 
-    void FixedUpdate(){
-        if(!view.IsMine) return;
+    void FixedUpdate()
+    {
+        if (!view.IsMine) return;
         // Movement
         var vel = rb.velocity;
         // Changing the velocity directly
@@ -75,14 +101,36 @@ public class PlayerMovement : MonoBehaviour {
         rb.velocity = vel;
 
         // Jumping
-        if(can_jump){
+        if (can_jump)
+        {
             rb.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
             can_jump = false;
+        }
+
+        // Hovering
+        if (isHovering)
+        {
+            rb.gravityScale = 0; // Disable gravity while hovering
+            rb.AddForce(Vector2.up * hoverForce, ForceMode2D.Force);
+            currentFuel -= fuelConsumptionRate * Time.fixedDeltaTime;
+            if (currentFuel < 0) currentFuel = 0;
+            Debug.Log("Hovering: Fuel remaining: " + currentFuel);
+        }
+        else
+        {
+            rb.gravityScale = gravity; // Restore gravity
+            if (currentFuel < maxFuel)
+            {
+                currentFuel += fuelRestoreRate * Time.fixedDeltaTime;
+                if (currentFuel > maxFuel) currentFuel = maxFuel;
+                Debug.Log("Not hovering: Fuel restoring: " + currentFuel);
+            }
         }
     }
 
     // Debugging
-    void OnDrawGizmos(){
+    void OnDrawGizmos()
+    {
         // Groundcheckbox
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(feetPos.position, groundCheckSize);
